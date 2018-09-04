eb38b0792ead7ce601a822db16091c31b9dd6641
Markus <markus@greenrobot>
2018-09-04 14:49:09 +0200

    conan-export.sh: check for obx-bintray remote and give advise on how to setup

--- conan-export.sh
+++ conan-export.sh
@@ -8,5 +8,13 @@ conan info .
 
 read -p "Upload to bintray? [y/N] " -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
+    obxRemote=$(conan remote list | grep obx-bintray) || true
+    echo "Using Conan remote: $obxRemote"
+    if [ -z "$obxRemote" ]; then
+        echo "The Conan remote \"obx-bintray\" was not set up set. Run this:"
+        echo "conan remote add obx-bintray https://api.bintray.com/conan/objectbox/conan"
+        echo "conan user -p \$password -r obx-bintray \$user"
+        exit 1
+    fi
     conan upload ${ref} --all -r=obx-bintray
 fi

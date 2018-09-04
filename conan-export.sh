375e257278f819225806242b3231d8245287f66c
Markus <markus@greenrobot>
2018-09-04 19:13:33 +0200

    conan-export.sh: test if export requires force, append new hashes to hashes file

--- conan-export.sh
+++ conan-export.sh
@@ -1,10 +1,37 @@
 #!/usr/bin/env bash
 set -e
 
-ref=objectbox-c/0.1@objectbox/testing
+version="0.1"
+ref=objectbox-c/${version}@objectbox/testing
+existingID=$(conan search ${ref} | grep "Package_ID:" | awk '{print $NF}')
+if [ -z "$existingID" ]; then
+    echo "$ref does not yet exist. Exporting..."
+    conan export-pkg . ${ref}
+else
+    echo "$ref already exist with package ID $existingID. Forcing export..."
+    conan export-pkg -f . ${ref}
+fi
+
 conan test . ${ref}
-conan export-pkg -f . ${ref}
-conan info .
+
+newID=$(conan info . | grep " ID:" | awk '{print $NF}')
+if [ -z "$newID" ]; then
+    echo "Could not grep ID from conan info. Output:"
+    conan info .
+    exit 1
+fi
+
+hashesFile=download.sh
+hashEntry=$(grep "$newID" ${hashesFile}) || true
+if [ -z "$hashEntry" ]; then
+    arch=$(uname -m)
+    os=$(uname)
+    hashEntry="# ${os}::${arch}::${version} ${newID}"
+    printf "\n$hashEntry" >> ${hashesFile}
+    echo "Added hash entry ($hashEntry) to ${hashesFile}"
+else
+    echo "Hash entry ($hashEntry) already present in ${hashesFile}"
+fi
 
 read -p "Upload to bintray? [y/N] " -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then

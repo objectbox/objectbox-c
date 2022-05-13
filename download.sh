406e7e221c575706838bc65c02001779df5f721f
Can Kolay <cankolay3499@gmail.com>
2022-05-13 20:36:47 +0300

    Revert "Fix if condition"
    This reverts commit b7e326f8d2272e4dbe5e7746eb356386a742fa76.

--- download.sh
+++ download.sh
@@ -18,26 +18,26 @@ variant=
 ###
 
 while :; do
-	case ${1:-} in
-	-h | --help)
-		printHelp=true
-		;;
-	--quiet)
-		quiet=true
-		;;
-	--install)
-		quiet=true
-		installLibrary=true
-		;;
-	--uninstall)
-		uninstallLibrary=true
-		;;
-	--sync)
-		variant="sync-"
-		;;
-	*) break ;;
-	esac
-	shift
+    case ${1:-} in
+        -h|--help)
+          printHelp=true
+        ;;
+        --quiet)
+          quiet=true
+        ;;
+        --install)
+          quiet=true
+          installLibrary=true
+        ;;
+        --uninstall)
+          uninstallLibrary=true
+        ;;
+        --sync)
+          variant="sync-"
+        ;;
+        *) break
+    esac
+    shift
 done
 
 tty -s || quiet=true
@@ -50,128 +50,128 @@ arch=${3:-$(uname -m)}
 echo "Base config: OS ${os} and architecture ${arch}"
 
 if [[ "$os" == MINGW* ]] || [[ "$os" == CYGWIN* ]]; then
-	os=Windows
-	echo "Adjusted OS to $os"
+    os=Windows
+    echo "Adjusted OS to $os"
 elif [[ "$os" == "Darwin" ]]; then
-	os=MacOS
-	arch=universal
-	echo "Adjusted OS to $os and architecture to $arch"
+    os=MacOS
+    arch=universal
+    echo "Adjusted OS to $os and architecture to $arch"
 elif [[ "$(uname -o)" == "Android" ]]; then
-	os=Android
-	echo "Adjusted OS to $os"
+    os=Android
+    echo "Adjusted OS to $os"
 fi
 
 if [[ $arch == "x86_64" ]]; then
-	arch=x64
+    arch=x64
 elif [[ $arch == armv7* ]]; then
-	arch=armv7hf
-	echo "Selected ${arch} architecture for download (hard FP only!)"
+    arch=armv7hf
+    echo "Selected ${arch} architecture for download (hard FP only!)"
 elif [[ $arch == armv6* ]]; then
-	arch=armv6hf
-	echo "Selected ${arch} architecture for download (hard FP only!)"
+    arch=armv6hf
+    echo "Selected ${arch} architecture for download (hard FP only!)"
 fi
 
 # sudo might not be defined (e.g. when building a docker image)
 sudo="sudo"
 if [ ! -x "$(command -v sudo)" ]; then
-	sudo=""
+    sudo=""
 fi
 
 # original location where we installed in previous versions of this script
 oldLibDir=
 
 if [[ "$os" = "MacOS" ]]; then
-	libFileName=libobjectbox.dylib
-	libDirectory=/usr/local/lib
+    libFileName=libobjectbox.dylib
+    libDirectory=/usr/local/lib
 elif [[ "$os" = "Windows" ]]; then
-	libFileName=objectbox.dll
+    libFileName=objectbox.dll
 
-	# this doesn't work in Git Bash, fails silently
-	# sudo="runas.exe /user:administrator"
-	# libDirectory=/c/Windows/System32
+    # this doesn't work in Git Bash, fails silently
+    # sudo="runas.exe /user:administrator"
+    # libDirectory=/c/Windows/System32
 
-	# try to determine library path based on gcc.exe path
-	libDirectory=""
-	if [ -x "$(command -v gcc)" ] && [ -x "$(command -v dirname)" ] && [ -x "$(command -v realpath)" ]; then
-		libDirectory=$(realpath "$(dirname "$(command -v gcc)")/../lib")
-	fi
-elif [[ "$os" = "Android" ]]; then
-	libFileName=libobjectbox.so
-	libDirectory=$PREFIX/lib
+    # try to determine library path based on gcc.exe path
+    libDirectory=""
+    if [ -x "$(command -v gcc)" ] && [ -x "$(command -v dirname)" ] && [ -x "$(command -v realpath)" ]; then
+        libDirectory=$(realpath "$(dirname "$(command -v gcc)")/../lib")
+    fi
+elif [[ "$os" = "Android"]]; then
+    libFileName=libobjectbox.so
+    libDirectory=$PREFIX/lib
 else
-	libFileName=libobjectbox.so
-	libDirectory=/usr/lib
-	oldLibDir=/usr/local/lib
+    libFileName=libobjectbox.so
+    libDirectory=/usr/lib
+    oldLibDir=/usr/local/lib
 fi
 
+
 function printUsage() {
-	echo "download.sh [\$1:version] [\$2:os] [\$3:arch]"
-	echo
-	echo "  Options (use at front only):"
-	echo "    --sync: download ObjectBox Sync variant of the library"
-	echo "    --quiet: skipping asking to install to ${libDirectory}"
-	echo "    --install: install library to ${libDirectory}"
-	echo "    --uninstall: uninstall from ${libDirectory}"
+    echo "download.sh [\$1:version] [\$2:os] [\$3:arch]"
+    echo
+    echo "  Options (use at front only):"
+    echo "    --sync: download ObjectBox Sync variant of the library"
+    echo "    --quiet: skipping asking to install to ${libDirectory}"
+    echo "    --install: install library to ${libDirectory}"
+    echo "    --uninstall: uninstall from ${libDirectory}"
 }
 
-if ${printHelp}; then
-	printUsage
-	exit 0
+if ${printHelp} ; then
+    printUsage
+    exit 0
 fi
 
 function uninstallLib() {
-	dir=${1}
+    dir=${1}
 
-	if ! [ -f "${dir}/${libFileName}" ]; then
-		echo "${dir}/${libFileName} doesn't exist, can't uninstall"
-		exit 1
-	fi
+    if ! [ -f "${dir}/${libFileName}" ] ; then
+        echo "${dir}/${libFileName} doesn't exist, can't uninstall"
+        exit 1
+    fi
 
-	echo "Removing ${dir}/${libFileName}"
+    echo "Removing ${dir}/${libFileName}"
 
-	if [ -x "$(command -v ldconfig)" ]; then
-		linkerUpdateCmd="ldconfig ${dir}"
-	else
-		linkerUpdateCmd=""
-	fi
+    if [ -x "$(command -v ldconfig)" ]; then
+        linkerUpdateCmd="ldconfig ${dir}"
+    else
+        linkerUpdateCmd=""
+    fi
 
-	$sudo bash -c "rm -fv '${dir}/${libFileName}' ; ${linkerUpdateCmd}"
+    $sudo bash -c "rm -fv '${dir}/${libFileName}' ; ${linkerUpdateCmd}"
 }
 
 if ${uninstallLibrary:-false}; then
-	uninstallLib "${libDirectory}"
+    uninstallLib "${libDirectory}"
 
-	if [ -x "$(command -v ldconfig)" ]; then
-		libInfo=$(ldconfig -p | grep "${libFileName}" || true)
-	else
-		libInfo=$(ls -lh ${libDirectory}/* | grep "${libFileName}" || true) # globbing would give "no such file" error
-	fi
+    if [ -x "$(command -v ldconfig)" ]; then
+        libInfo=$(ldconfig -p | grep "${libFileName}" || true)
+    else
+        libInfo=$(ls -lh ${libDirectory}/* | grep "${libFileName}" || true) # globbing would give "no such file" error
+    fi
 
-	if [ -z "${libInfo}" ]; then
-		echo "Uninstall successful"
-	else
-		echo "Uninstall failed, leftover files:"
-		echo "${libInfo}"
-		exit 1
-	fi
+    if [ -z "${libInfo}" ]; then
+        echo "Uninstall successful"
+    else
+        echo "Uninstall failed, leftover files:"
+        echo "${libInfo}"
+        exit 1
+    fi
 
-	exit 0
+    exit 0
 fi
 
 downloadDir=download
 
-while getopts v:d: opt; do
-	case $opt in
-	d) downloadDir=$OPTARG ;;
-	v) version=$OPTARG ;;
-	*)
-		printUsage
-		exit 1
-		;;
-	esac
+while getopts v:d: opt
+do
+    case $opt in
+        d) downloadDir=$OPTARG;;
+        v) version=$OPTARG;;
+        *) printUsage
+           exit 1 ;;
+   esac
 done
 
-conf=$(echo "${os}-${arch}" | tr '[:upper:]' '[:lower:]') # convert to lowercase
+conf=$(echo "${os}-${arch}" | tr '[:upper:]' '[:lower:]')   # convert to lowercase
 
 SUPPORTED_PLATFORMS="
 linux-x64
@@ -182,22 +182,22 @@ windows-x86
 windows-x64
 macos-universal
 " #SUPPORTED_PLATFORMS
-if [ -z "$(awk -v key="${conf}" '$1 == key {print $NF}' <<<"$SUPPORTED_PLATFORMS")" ]; then
-	echo "Warning: platform configuration ${conf} is not listed as supported."
-	echo "Trying to continue with the download anyway, maybe the list is out of date."
-	echo "If that doesn't work you can select the configuration manually (use --help for details)"
-	echo "Possible values are:"
-	awk '$0 {print " - " $1 }' <<<"$SUPPORTED_PLATFORMS"
-	exit 1
+if [ -z "$( awk -v key="${conf}" '$1 == key {print $NF}' <<< "$SUPPORTED_PLATFORMS" )" ]; then
+    echo "Warning: platform configuration ${conf} is not listed as supported."
+    echo "Trying to continue with the download anyway, maybe the list is out of date."
+    echo "If that doesn't work you can select the configuration manually (use --help for details)"
+    echo "Possible values are:"
+    awk '$0 {print " - " $1 }'  <<< "$SUPPORTED_PLATFORMS"
+    exit 1
 fi
 
 conf="${variant}${conf}"
 echo "Using configuration ${conf}"
 
-if [[ "$os" == "Linux" ]]; then
-	archiveExt=tar.gz
+if [[ "$os" == "Linux"  ]]; then
+  archiveExt=tar.gz
 else
-	archiveExt=zip
+  archiveExt=zip
 fi
 
 targetDir="${downloadDir}/objectbox-${version}-${conf}"
@@ -209,14 +209,14 @@ mkdir -p "$(dirname "${archiveFile}")"
 
 # Support both curl and wget because their availability is platform dependent
 if [ -x "$(command -v curl)" ]; then
-	curl --location --fail --output "${archiveFile}" "${downloadUrl}"
+    curl --location --fail --output "${archiveFile}" "${downloadUrl}"
 else
-	wget --no-verbose --output-document="${archiveFile}" "${downloadUrl}"
+    wget --no-verbose --output-document="${archiveFile}" "${downloadUrl}"
 fi
 
 if [[ ! -s ${archiveFile} ]]; then
-	echo "Error: download failed (file ${archiveFile} does not exist or is empty)"
-	exit 1
+    echo "Error: download failed (file ${archiveFile} does not exist or is empty)"
+    exit 1
 fi
 
 echo "Downloaded:"
@@ -227,68 +227,68 @@ echo "Extracting into ${targetDir}..."
 mkdir -p "${targetDir}"
 
 if [[ "$archiveExt" == "zip" ]]; then
-	unzip "${archiveFile}" -d "${targetDir}"
+  unzip "${archiveFile}" -d "${targetDir}"
 else
-	tar -xzf "${archiveFile}" -C "${targetDir}"
+  tar -xzf "${archiveFile}" -C "${targetDir}"
 fi
 
-if [ ! -d "${libBuildDir}" ] || ${quiet}; then
-	mkdir -p "${libBuildDir}"
-	cp "${targetDir}"/lib/* "${libBuildDir}"
-	echo "Copied to ${libBuildDir}:"
-	ls -l "${libBuildDir}"
+if [ ! -d "${libBuildDir}"  ] || ${quiet} ; then
+    mkdir -p "${libBuildDir}"
+    cp "${targetDir}"/lib/* "${libBuildDir}"
+    echo "Copied to ${libBuildDir}:"
+    ls -l "${libBuildDir}"
 else
-	read -p "${libBuildDir} already exists. Copy the just downloaded library to it? [Y/n] " -r
-	if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z "$REPLY" ]]; then
-		cp "${targetDir}"/lib/* "${libBuildDir}"
-		echo "Copied; contents of ${libBuildDir}:"
-		ls -l "${libBuildDir}"
-	fi
+    read -p "${libBuildDir} already exists. Copy the just downloaded library to it? [Y/n] " -r
+    if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z "$REPLY" ]] ; then
+        cp "${targetDir}"/lib/* "${libBuildDir}"
+        echo "Copied; contents of ${libBuildDir}:"
+        ls -l "${libBuildDir}"
+    fi
 fi
 
-if ${quiet}; then
-	if ! ${installLibrary:-false}; then
-		echo "Skipping installation to ${libDirectory} in quiet mode"
-		if [ -f "${libDirectory}/${libFileName}" ]; then
-			echo "However, you have a library there:"
-			ls -l "${libDirectory}/${libFileName}"
-		fi
-	fi
+if ${quiet} ; then
+    if ! ${installLibrary:-false}; then
+        echo "Skipping installation to ${libDirectory} in quiet mode"
+        if [ -f "${libDirectory}/${libFileName}" ]; then
+            echo "However, you have a library there:"
+            ls -l "${libDirectory}/${libFileName}"
+        fi
+    fi
 else
-	if [ -z "${installLibrary:-}" ] && [ -n "${libDirectory}" ]; then
-		read -p "OK. Do you want to install the library into ${libDirectory}? [Y/n] " -r
-		if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z "$REPLY" ]]; then
-			installLibrary=true
+    if [ -z "${installLibrary:-}" ] && [ -n "${libDirectory}" ]; then
+        read -p "OK. Do you want to install the library into ${libDirectory}? [Y/n] " -r
+        if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z "$REPLY" ]] ; then
+            installLibrary=true
 
-			if [ -n "${oldLibDir}" ] && [ -f "${oldLibDir}/${libFileName}" ]; then
-				echo "Found an old installation in ${oldLibDir} but a new one is going to be placed in ${libDirectory}."
-				echo "It's recommended to uninstall the old library to avoid problems."
-				read -p "Uninstall from old location? [Y/n] " -r
-				if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z "$REPLY" ]]; then
-					uninstallLib ${oldLibDir}
-				fi
-			fi
+            if [ -n "${oldLibDir}" ] && [ -f "${oldLibDir}/${libFileName}" ] ; then
+                echo "Found an old installation in ${oldLibDir} but a new one is going to be placed in ${libDirectory}."
+                echo "It's recommended to uninstall the old library to avoid problems."
+                read -p "Uninstall from old location? [Y/n] " -r
+                if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z "$REPLY" ]] ; then
+                    uninstallLib ${oldLibDir}
+                fi
+            fi
 
-		fi
-	fi
+        fi
+    fi
 fi
 
 if ${installLibrary:-false}; then
-	echo "Installing ${libDirectory}/${libFileName}"
-	$sudo cp "${targetDir}/lib/${libFileName}" ${libDirectory}
+    echo "Installing ${libDirectory}/${libFileName}"
+    $sudo cp "${targetDir}/lib/${libFileName}" ${libDirectory}
 
-	if [ -x "$(command -v ldconfig)" ]; then
-		$sudo ldconfig "${libDirectory}"
-		libInfo=$(ldconfig -p | grep "${libFileName}" || true)
-	else
-		libInfo=$(ls -lh ${libDirectory}/* | grep "${libFileName}" || true) # globbing would give "no such file" error
-	fi
+    if [ -x "$(command -v ldconfig)" ]; then
+        $sudo ldconfig "${libDirectory}"
+        libInfo=$(ldconfig -p | grep "${libFileName}" || true)
+    else
+        libInfo=$(ls -lh ${libDirectory}/* | grep "${libFileName}" || true) # globbing would give "no such file" error
+    fi
 
-	if [ -z "${libInfo}" ]; then
-		echo "Error installing the library - not found"
-		exit 1
-	else
-		echo "Installed objectbox libraries:"
-		echo "${libInfo}"
-	fi
+    if [ -z "${libInfo}" ]; then
+        echo "Error installing the library - not found"
+        exit 1
+    else
+        echo "Installed objectbox libraries:"
+        echo "${libInfo}"
+    fi
 fi
